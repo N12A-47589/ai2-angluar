8fa33d2516d5e75fc3b461d4e4a6e3d4122ac6ac
Wiktoria <wiktorialopatka222@gmail.com>
2024-01-18 14:18:01 +0100

    poprawione

--- lab-d/src/app/app.module.ts
+++ lab-d/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ArchiveComponent } from './archive/archive.component';
 
 @NgModule({
-  declarations: [AppComponent, TasksComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [AppComponent, TasksComponent, ArchiveComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- lab-d/src/app/tasks/tasks.component.ts
+++ lab-d/src/app/tasks/tasks.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Task } from '../task';
 import { TasksService } from '../tasks.service';
+import { FormsModule } from '@angular/forms';
+import { Observable, forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-tasks',
@@ -10,6 +12,7 @@ import { TasksService } from '../tasks.service';
 export class TasksComponent implements OnInit {
   tasks: Task[] = [];
   newTask: Task = {};
+  public isProcessing = false;
 
   constructor(private tasksService: TasksService) {}
 
@@ -20,6 +23,46 @@ export class TasksComponent implements OnInit {
   private getTasks(): void {
     this.tasksService.index().subscribe((tasks) => {
       this.tasks = tasks;
+    });
+    this.isProcessing = false;
+  }
+
+  addTask() {
+    if (this.newTask.title === undefined) {
+      return;
+    }
+
+    this.newTask.completed = false;
+    this.newTask.archived = false;
+
+    this.tasks.unshift(this.newTask);
+
+    this.tasksService.post(this.newTask).subscribe((task) => {
+      this.newTask = {};
+      this.ngOnInit();
+    });
+  }
+  handleChange(task: Task) {
+    this.tasksService.put(task).subscribe({
+      error: (err) => {
+        alert(err);
+        this.ngOnInit();
+      },
+    });
+  }
+  archiveCompleted() {
+    const observables: Observable<any>[] = [];
+    for (const task of this.tasks) {
+      if (!task.completed) {
+        continue;
+      }
+
+      task.archived = true;
+      observables.push(this.tasksService.put(task));
+    }
+
+    forkJoin(observables).subscribe(() => {
+      this.ngOnInit();
     });
   }
 }
